e21b15ce202dbb6ad05e1b1ae61661efa422be4a
alejandrodh <alejandro@digitalhouse.com>
2022-10-30 13:59:39 -0300

    Agregando datos en una colección. Likes

---
+++ clase19_likes/src/components/Post.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import { Text, View, TouchableOpacity } from 'react-native';
+
+import {auth, db} from '../firebase/config';
+import firebase from 'firebase';
+
+class Post extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            cantidadDeLikes:this.props.postData.data.likes.length, //length del array de likes.
+            miLike:false
+        }
+    }
+
+    componentDidMount(){
+        //chequear si el email del usuario logueado está en el array. El usuario logueado se obtiene de auth.currentUser.email. Chequear que este importado auth.
+        //Si está voy a cambiar el estado miLike.
+        if(this.props.postData.data.likes.includes('nico@dh.com')){ 
+            this.setState({
+                miLike:true
+            })
+        }
+    }
+
+    like(){
+        //agregar el email del usuario logueado a un array en el posteo.
+        db.collection('posts')
+            .doc(this.props.postData.id) //identificar el documento
+            .update({
+                likes: firebase.firestore.FieldValue.arrayUnion('nico@dh.com') //traer el email del usuario logueado con auth.currentUser.email. Chequear que este importado auth.
+            })
+            .then(()=> this.setState({
+                cantidadDeLikes: this.state.cantidadDeLikes +1,
+                miLike: true, 
+                })
+            )
+            .catch(e=>console.log(e))
+    }
+
+    unlike(){
+        //tarea para la casa.
+    }
+
+    render(){
+        console.log(this.props);
+        return(
+            <View>
+                <Text> {this.props.postData.data.textoPost} </Text>
+                <Text> Cantidad de Likes: {this.state.cantidadDeLikes} </Text>
+                { this.state.miLike ? 
+                    <TouchableOpacity onPress={ ()=> this.unlike() }>
+                        <Text>No me gusta más</Text>
+                    </TouchableOpacity>
+                    :
+                    <TouchableOpacity onPress={ ()=> this.like() }>
+                        <Text>Me gusta</Text>
+                    </TouchableOpacity>
+                }
+            </View>
+        )
+    }
+}
+
+export default Post;
---
+++ clase19_likes/src/screens/NewPost.js
@@ -0,0 +1,55 @@
+import React, {Component} from 'react'
+import {Text, TextInput, TouchableOpacity,View} from 'react-native';
+import {auth, db} from '../firebase/config';
+
+
+class NewPost extends Component{
+    constructor(){
+        super()
+        this.state={
+            textoPost:'',
+            createdAt:'',
+            photo:''
+        }
+    }
+
+    createPost(texto, photo){
+        db.collection('posts').add({
+                owner: 'facu@dh.com', //deberia ser el usuario registrado. auth.currentUser
+                textoPost: texto,
+                photo: photo,
+                likes:[],
+                comments:[],
+                createdAt: Date.now()
+            })
+            .then(() => {
+                this.setState({
+                    texto:'',
+                })
+                this.props.navigation.navigate('Home')
+            })
+            .catch( e => console.log(e))
+    }
+
+    render(){
+        return(
+            <View>
+                <Text> Nuevo posteo form</Text>
+                <View>
+                    <TextInput  
+                        placeholder='texto post'
+                        keyboardType='default'
+                        //poner propiedad para transformarlo en textArea
+                        onChangeText={ text => this.setState({textoPost:text}) }
+                        value={this.state.textoPost}
+                    /> 
+                    <TouchableOpacity onPress={()=>this.createPost(this.state.textoPost, this.state.photo)}>
+                        <Text>Guardar</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        )
+    }
+}
+
+export default NewPost;
